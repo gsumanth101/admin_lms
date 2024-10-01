c0435adc488597f1b769b9c599650674bbfd54be
bhathubablu <bhathubablu@gmail.com>
2024-10-02 00:40:55 +0530

    api

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/api';
+// const API_BASE_URL = 'http://localhost:3000/api';
+
+const API_BASE_URL = 'https://api.phemesoft.com/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
